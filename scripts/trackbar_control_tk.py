4d3c9314b29483347aed91ace2f916b0c9878cba
khairulizwan <wansnap@gmail.com>
2020-04-02 15:47:16 +0800

    Updated on 04-02-2020-02:22:06 from 172.20.10.2 : khairulizwan-MacBookPro

--- scripts/trackbar_control_tk.py
+++ scripts/trackbar_control_tk.py
@@ -18,42 +18,61 @@ from geometry_msgs.msg import Twist
 
 from Tkinter import *
 
-rospy.init_node('tank_teleop')
-pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+e = """
+Communications Failed
+"""
 
-master = Tk()
+if __name__=="__main__":
+	rospy.init_node('tank_teleop')
+	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-master.title('The Controller')
-#You can set the geometry attribute to change the root windows size
-master.geometry("70x100") #You want the size of the app to be 500x500
-master.resizable(0, 0) #Don't allow resizing in the x or y direction
+	try:
+		master = Tk()
+
+		master.title('The Controller')
+		#You can set the geometry attribute to change the root windows size
+		master.geometry("70x100") #You want the size of the app to be 500x500
+		master.resizable(0, 0) #Don't allow resizing in the x or y direction
 
 
-leftValue = IntVar()  # IntVars to hold
-rightValue = IntVar() # values of scales
+		leftValue = IntVar()  # IntVars to hold
+		rightValue = IntVar() # values of scales
 
-leftScale = Scale(master, from_=-255, to=255, variable=leftValue, showvalue=0)
-leftScale.set(0)
+		leftScale = Scale(master, from_=-255, to=255, variable=leftValue, showvalue=0)
+		leftScale.set(0)
 
-rightScale = Scale(master, from_=-255, to=255, variable=rightValue, showvalue=0)
-rightScale.set(0)
+		rightScale = Scale(master, from_=-255, to=255, variable=rightValue, showvalue=0)
+		rightScale.set(0)
 
-leftLabel = Label(master, textvariable=leftValue, wraplength=1)   # labels that will update
-rightLabel = Label(master, textvariable=rightValue, wraplength=1) # with IntVars as slider moves
+		leftLabel = Label(master, textvariable=leftValue, wraplength=1)   # labels that will update
+		rightLabel = Label(master, textvariable=rightValue, wraplength=1) # with IntVars as slider moves
 
-leftLabel.grid(row=0, column=0)
-leftScale.grid(row=0, column=1)
-rightLabel.grid(row=0, column=3)
-rightScale.grid(row=0, column=2)
+		leftLabel.grid(row=0, column=0)
+		leftScale.grid(row=0, column=1)
+		rightLabel.grid(row=0, column=3)
+		rightScale.grid(row=0, column=2)
 
-twist = Twist()
-twist.linear.x = leftValue
-twist.linear.y = 0.0
-twist.linear.z = 0.0
+		while(1):
+			twist = Twist()
+			twist.linear.x = leftValue
+			twist.linear.y = 0.0
+			twist.linear.z = 0.0
 
-twist.angular.x = 0.0
-twist.angular.y = 0.0
-twist.angular.z = rightValue
-pub.publish(twist)
+			twist.angular.x = 0.0
+			twist.angular.y = 0.0
+			twist.angular.z = rightValue
+			pub.publish(twist)
 
-mainloop()
+		mainloop()
+
+	except:
+		print(e)
+
+	finally:
+		twist = Twist()
+		twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+		twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+		pub.publish(twist)
+
+		master.quit()
+
